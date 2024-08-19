6ac932baebcd66b14663fbf0d424a72c8465d8fa
harshjain2606 <jaainharsh383@gmail.com>
2024-08-19 16:23:22 +0530

    push

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -75,7 +75,7 @@ const HomePage = () => {
       try {
         const user = JSON.parse(localStorage.getItem("user"));
         setLoading(true);
-        const res = await axios.post("/transections/get-transection", {
+        const res = await axios.post("/api/v1/transections/get-transection", {
           userid: user._id,
           frequency,
           selectedDate,
@@ -96,7 +96,7 @@ const HomePage = () => {
   const handleDelete = async (record) => {
     try {
       setLoading(true);
-      await axios.post("/transections/delete-transection", {
+      await axios.post("/api/v1/transections/delete-transection", {
         transacationId: record._id,
       });
       setLoading(false);
@@ -114,7 +114,7 @@ const HomePage = () => {
       const user = JSON.parse(localStorage.getItem("user"));
       setLoading(true);
      if (editable) {
-      await axios.post("/transections/edit-transection", {
+      await axios.post("/api/v1/transections/edit-transection", {
         payload: {
           ...values,
           userId: user._id,
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const morgan = require("morgan");
 const dotenv = require("dotenv");
 const colors = require("colors");
 const connectDb = require("./config/connectDb");
+const path =require('path');
 
 dotenv.config();
 //databse call
@@ -22,6 +23,11 @@ app.use("/api/v1/users", require("./routes/userRoute"));
 
 //transection routes
 app.use("/api/v1/transections", require("./routes/transectionRoutes"));
+//static files
+app.use(express.static(path.join(__dirname,'./client/build')));
+app.get('*', function(req,res){
+  res.sendFile(path.join(__dirname,"./client/build/index.html"));
+})
 
 //port
 const PORT = 8080 || process.env.PORT;
